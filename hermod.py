7cfa1f7d8da1e4322ed8f6a45bc36bbb82bfbff1
root <root@hermod.hacktor.com>
2019-08-19 21:25:26 +0000

    Switched to python for the IRC bot. Perl still used for the webhook

---
+++ hermod.py
@@ -0,0 +1,66 @@
+#!/usr/bin/python
+
+import socket
+import ssl
+import time
+import requests
+import urllib
+import json
+
+## Settings
+with open('/etc/hermod.json') as f:
+    config = json.load(f)
+
+irc_C = socket.socket(socket.AF_INET, socket.SOCK_STREAM) #defines the socket
+irc = ssl.wrap_socket(irc_C)
+
+print "Establishing connection to [%s]" % (config['ircnode'])
+# Connect
+irc.connect((config['ircnode'], config['port']))
+irc.setblocking(False)
+irc.send("PASS %s\n" % (config['password']))
+irc.send("USER "+ config['nick'] +" "+ config['nick'] +" "+ config['nick'] +" :herMod-Test\n")
+irc.send("NICK "+ config['nick'] +"\n")
+irc.send("PRIVMSG nickserv :identify %s %s\r\n" % (config['nick'], config['password']))
+irc.send("JOIN "+ config['channel'] +"\n")
+
+
+firstcall = True
+f = open(config['filename'], 'r')
+
+while True:
+    time.sleep(1)
+
+    # Tail Files
+    try:
+        if firstcall:
+            f.seek(0,2)
+            firstcall = False
+
+        line = f.readline()
+        if line != '':
+            irc.send("PRIVMSG %s :%s" % (config['channel'], line))
+            print "Send to IRC: " + line
+    except Exception as e:
+        print "Error with file %s" % (config['filename'])
+        print e
+
+    try:
+        buf = irc.recv(2040)
+        
+        texts = buf.splitlines()
+        for text in texts:
+            print text
+
+            if text.find('PRIVMSG ' + config['channel']) != -1:
+                nick = text.partition('!')[0].strip(':')
+                msg = "IRC Msg by " + nick + ": " + text.rpartition('PRIVMSG ' + config['channel'] + ' :')[2]
+                print "send to telegram: " + msg
+                URL = "https://api.telegram.org/bot" + config['token'] + "/sendMessage?chat_id=" + config['chat_id'] + "&text=" + urllib.quote(msg)
+                requests.get(URL)
+
+            # Prevent Timeout
+            if text.find('PING') != -1:
+                irc.send('PONG ' + text.split() [1] + '\r\n')
+    except Exception:
+        continue
